c216bb19a161b1244a4fec7726596d721c6ea895
amnorian <amnorian@hotmail.com>
2020-12-16 12:56:07 +0100

    création d'1 fonction JS et usage de la fonction

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-var balise=document.queryselector('#jsload');
+//declaration d'une fonction
+funtion initialisationJS(){
+var jsload=document.querySelector('#jsload');
+jsload.innerHTML='mon <span style="font-weight:900"> JS </span> est chargé';
+jsload.style.backgroundColor='LIME';
+}
+
+//usage d'une fonction
+initialisationJS();
