377ee376f5410f55524761ad6740dd218cbca657
amnorian <amnorian@hotmail.com>
2020-12-16 16:12:18 +0100

    ajout du postit et création de l'image close sur le post it

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function initialisationJS(prenom) {
     jsload.style.backgroundColor = 'LIME';
 }
 
-document.addEventListener('load', function (evt) {
+addEventListener('load', function (evt) {
     //usage d'une fonction
     initialisationJS('Rémy');
     document.querySelector('form').addEventListener('submit', formSubmitted);
@@ -14,15 +14,23 @@ document.addEventListener('load', function (evt) {
 
 
 function formSubmited(evt) {
-    evt.preventDefault();
+    evt.preventDefault(); //arret du rechargement de la page qui supprimerait le post-it
     console.log('mon formulaire est "submit" ');
     console.log(evt.target[0].value);
     console.log(evt.target[1].value);
     console.log(evt.target[2].value);
     console.log(evt.target[3].value);
+    var monFormulaire = document.forms['editor-form'];
+    //var dateFormated=moment(monFormulaire['date'].value,'DD MM YYYY')
+
+    createPostit(
+        monFormulaire['titre'].value,
+        monFormulaire['date'].value,
+        monFormulaire['time'].value,
+        monFormulaire['description'].value);
 }
 document.querySelector('form').addEventListener('submit', formSubmited); //document permet de selectionner tous le document, queryselector pour aller chercher le formulaire form
-//addevelistener(ce que l'on veut faire (ici submità, la function lancée sans les parenthèse car on ne veut pas l'executer))
+//addevelistener(ce que l'on veut faire (ici submit à, la function lancée sans les parenthèse car on ne veut pas l'executer))
 
 
 
@@ -32,16 +40,21 @@ document.querySelector('form').addEventListener('submit', formSubmited); //docum
  * @param {string} titre titre de la note
  * @param {String} date date ISO AAA-MM*JJ pour la note
  * @param {String} heure heure ISO HH:MM:SS pour la note
- * @param {*String} description descritpion de la note
+ * @param {*String} description description de la note
  */
 function createPostit(titre, date, heure, description) {
     var postit = document.createElement('div');
     postit.classList.add('postit');
-    postit.innerHTML ='<div class="postit-titre">' +titre+ '</div> date : <span class="datetime">'+date+' </span> heure : <span class:"datetime">'+heure+'</span>\
-        <h2>Description</h2>'+description; 
-        //pour sauter les lignes on met un "\"
+    postit.innerHTML = '<div class="close"><img src="img/Close.png"/> \
+    </div><div class="postit-titre">' + titre + '</div> date : <span class="datetime">' + date + ' </span> heure : <span class:"datetime">' + heure + '</span>\
+        <h2>Description</h2>'+ description;
+    //pour sauter les lignes on met un "\"
     var liste = document.querySelector('#list');
     liste.append(postit);
 }
-
 /** exemple de lancement de la fonction : createPostit('mon titre','2020-01-02','00:00:00', 'description du content') */
+
+function deletePostit(evt) {
+    console.log('evenement lié à la suppresion d\'une note', evt);
+
+}
